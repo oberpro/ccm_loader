5d4aec95cda39ac6ae02a03ef65214654cb1e08a
Kevin Kirch <git-laptop@oberpro.de>
2018-06-07 15:50:56 +0200

    added first solution to git

---
+++ ccm_loader.js
@@ -0,0 +1,81 @@
+if (!window.ccm_loader) ccm_loader = {
+
+    makeUrl: function (url, newVersion) {
+        var first = (url + '').substring(0, url.lastIndexOf("/") + 1);
+        var last = url.substring(url.lastIndexOf("/") + 1, url.length);
+        var part = last.split("-");
+        if (part.length > 1) {
+            var version = part[1];
+            version = version.substring(0, version.indexOf('.js'));
+            if (newVersion === 'latest') {
+                part.splice(1, 1);
+            } else {
+                part[1] = newVersion;
+            }
+        } else {
+            part = [];
+            part.push("ccm");
+            if (newVersion !== 'latest') {
+                part.push(newVersion);
+            }
+        }
+        return first + part.join("-") + ".js";
+    },
+    resolve: function (component) {
+        var v = component.ccm.url.split("/").pop().split("-");
+        v.length > 1 ? (v = v[1].split("."), v.pop(), "min" === v[v.length - 1] && v.pop(), v = v.join(".")) : v = "latest";
+        if (window.ccm_version) {
+            //has version
+            var min_version = window.ccm_version;
+            var prefix = min_version.charAt(0);
+            if (prefix === '+' || prefix === '^') {
+                var vers = min_version.substring(1).split('.');
+                if (vers.length > 0) {
+                    vers = vers.map(v => parseInt(v));
+                    if (window.ccm_loader.matchVersion(vers, v.split('.').map(v => parseInt(v)))) {
+                        //ok load
+                        console.log("matched", vers, "and", v);
+                        window.ccm_loader.load(component, v);
+                    } else {
+                        console.log("load fixed version", min_version.substring(1));
+                        //load fixed version
+                        window.ccm_loader.load(component, min_version.substring(1));
+                    }
+                } else {
+                    console.log("failed");
+                    window.ccm_loader.load(component, v);
+                }
+            } else {
+                //load fixed version
+                window.ccm_loader.load(component, min_version);
+            }
+        } else {
+            //load all because no limitation
+            window.ccm_loader.load(component, v);
+        }
+    },
+    add: function (v, component) {
+        console.log("component", component.name, "added to", v);
+        window.ccm[v].component(component);
+    },
+    matchVersion: function (limit, wanted) {
+        for (var i = 0; i < Math.min(limit.length, wanted.length); i++) {
+            if (limit[i] > wanted[i]) {
+                return false;
+            }
+        }
+        return true;
+    },
+    load: function (component, v) {
+        var url = window.ccm_loader.makeUrl(component.ccm.url, v);
+        console.log("new url", url);
+        if (window.ccm && window.ccm[v]) {
+            window.ccm_loader.add(v, component);
+        } else {
+            var e = document.createElement("script");
+            document.head.appendChild(e), component.ccm.integrity && e.setAttribute("integrity", component.ccm.integrity), component.ccm.crossorigin && e.setAttribute("crossorigin", component.ccm.crossorigin), e.onload = function () {
+                window.ccm_loader.add(v, component), document.head.removeChild(e);
+            }, e.src = url;
+        }
+    }
+};
---
+++ include-in-ccm-component.js
@@ -0,0 +1,24 @@
+var f = "ccm." + component.name + (component.version ? "-" + component.version.join(".") : "") + ".js";
+if (window.ccm && null === window.ccm.files[f]) window.ccm.files[f] = component;
+else {
+    var n = window.ccm && window.ccm.components[component.name];
+    n && n.ccm && (component.ccm = n.ccm), "string" == typeof component.ccm && (component.ccm = {
+        url: component.ccm
+    });
+    // NEW VERSION
+    (function l(obj) {
+        if (!window.ccm_loader) {
+            var e = document.createElement("script");
+            document.head.appendChild(e);
+            e.onload = function () {
+                resolve();
+            };
+            e.src = "https://oberpro.github.io/ccm_loader/ccm_loader.js";
+        } else {
+            resolve();
+        }
+        var resolve = function () {
+            window.ccm_loader.resolve(obj);
+        }
+    })(component);
+}
